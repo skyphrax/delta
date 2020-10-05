eb3eb5f5c1000a20e7b52d38af948bd0d2137ba6
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-05 15:37:42 +0200

    Update userinfo.js

--- Commandes/userinfo.js
+++ Commandes/userinfo.js
@@ -5,7 +5,7 @@ const colours = require("../colours.json")
 module.exports.run = async (client, message, args) => {
     if (!message.guild)
     return message.channel.send("Une erreur s'est produite !");
-    message.reply("Syntaxe: d!ui <entrer l'utilisateur mentionnnez>");
+    return message.reply("Syntaxe: d!ui <entrer l'utilisateur mentionnnez>");
     const user_mention = message.mentions.users.first();
     const statusgame = {
         null: ":x: Aucun jeu"
