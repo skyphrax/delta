552a26e0819e344ae2437d8eddecc01106ebeef1
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-05 14:42:40 +0200

    Update ping.js

--- Commandes/ping.js
+++ Commandes/ping.js
@@ -4,6 +4,8 @@ const {PREFIX} = require("../config.js");
 module.exports.run = (client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setTitle(`Voici votre ping : ${client.ws.ping} ms`)
+    . setTimestamp ( )
+    . setFooter ( `Tout droit résérvé | DeltaBot © ️ 2020` ,  client . user . displayAvatarURL )
         return message.channel.send(embed)
 }
 
