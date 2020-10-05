8ee4a694a68b0283339cfbf27f2ab23ba93fc16b
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-05 14:41:04 +0200

    Update clear.js

--- Commandes/clear.js
+++ Commandes/clear.js
@@ -11,9 +11,10 @@ module.exports.run = async (bot, message, args) => {
     message.channel.bulkDelete(args[0]).then(() => {
         
         let clearEmbed = new Discord.MessageEmbed()
-        .setDescription("~Clear~")
         .setColor(colours.red_light)
         .addField("Vous avez supprimer ", `${args[0]} message(s)`)
+        . setTimestamp ( )
+    . setFooter ( `Tout droit résérvé | DeltaBot © ️ 2020` ,  client . user . displayAvatarURL )
 
         message.channel.send(clearEmbed).then(async msg => {
             msg.delete({timeout: 5000})
