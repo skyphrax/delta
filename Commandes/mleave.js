fe2fc491e0418183944ad0d818d4264a2489d54a
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 09:41:17 +0200

    Delete mleave.js

--- Commandes/mleave.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const colours = require("../colours.json");
-
-module.exports.run = async (client, message, args) => {
-  const voicechannel = message.member.voice.channel;
-  const musicPlayer = client.musicPlayer.get(message.guild.id);
-  
-  const destroyMusicClient = id
-}
