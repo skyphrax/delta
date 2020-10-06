007b37a239dee7c57a06baed59f94a80bccbb724
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 10:13:44 +0200

    Create musicvolume.js

---
+++ Commandes/musicvolume.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const colours = require("../colours.json");
+
+module.exports.run = async (client, message, args) => {
+  const voicechannel = message.member.voice.channel;
+  const musicPlayer = client.musicPlayer.get(message.guild.id);
+  
+  if(!(musicPlayer && voiceChannel) && (musicPlayer.voiceChannel.id === voiceChannel.id) message.channel.send("Veuillez rejoindre le même salon que le bot!"));
+  
+  if(!isNaN(args[0]) && (args[0] > 0 && args[0] < 100)) {
+    musicPlayer.setVolume(args[0]);
+    message.channel.send(`Le volume est maintenant: ${args[0]}`);
+  } else {
+    message.channel.send("Merci de choisir un nombre entre 1 et 100!");
+  } else {
+    message.channel.send(`Le volume est actuellement: ${musicPlayer.volume}.`);
+}
