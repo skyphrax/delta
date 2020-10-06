de6ce520a5dea85d7f85ae4b5529800a4eacb75d
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 12:52:59 +0200

    Update guildCreate.js

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
-const { MessageEmbed } = require ('discord.js')
 
 module.exports = async (client, guild) => {
   let createdChannel = await guild.channels.create("Delta-Log", {type: "text"})
   createdChannel.send({
-    const exampleEmbed = new Discord.MessageEmbed()
+	  const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('test')
+	  
+	channel.send(exampleEmbed);
   })
   
   
