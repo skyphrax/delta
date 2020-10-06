e3954881fbfd37409226f0fa733d9d28993670a4
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 11:07:18 +0200

    Update guildCreate.js

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
+const {PREFIX} = require("../config.js");
+
 module.exports = async (guild) => {
   guild.channels.create({name: "Delta-log", type: "text"})
 }
