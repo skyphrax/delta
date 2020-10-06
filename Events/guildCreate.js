984efdf259624817d22fc922a31bfaec81a0ee48
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 13:05:29 +0200

    Update guildCreate.js

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -3,10 +3,7 @@ const Discord = require('discord.js');
 module.exports = async (client, guild) => {
   let createdChannel = await guild.channels.create("Delta-Log", {type: "text"})
   let embed = new Discord.MessageEmbed()
-  	.setcolor("#f94343")
 	.setDescription("Se salon a été créer pour config le bot")
-  	.setTimestamp()
-  	.setFooter(`Tout droit résérvé | DeltaBot ©️ 2020`, client.user.displayAvatarURL)
 
 	createdChannel.send(embed)
   
