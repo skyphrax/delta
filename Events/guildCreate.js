ce91888ea6d8a4b437fc6fe6eb89b3289eb3926d
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 12:46:18 +0200

    Update guildCreate.js

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js');
+const { MessageEmbed } = require ('discord.js')
 
 module.exports = async (client, guild) => {
   let createdChannel = await guild.channels.create("Delta-Log", {type: "text"})
-  createdChannel.send("Se salon a été créer pour config le bot")
+  createdChannel.send({
+    const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('test')
+  })
+  
   
   createdChannel.overwritePermissions([
     {
