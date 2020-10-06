80545bbcb6757241f6cc57d33aba14503a61a5d5
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 09:27:48 +0200

    Update mleave.js

--- Commandes/mleave.js
+++ Commandes/mleave.js
@@ -1,1 +1,7 @@
+const Discord = require("discord.js");
+const colours = require("../colours.json");
 
+module.exports.run = async (client, message, args) => {
+  const voicechannel = message.member.voice.channel;
+  const musicplayer
+}
