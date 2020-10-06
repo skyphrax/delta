e64f490a4b77c2072dedfda99f9f716bec12aca5
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-06 12:02:01 +0200

    Update guildCreate.js

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = async (client, guild) => {
   client.message.channel.send("Se salon est pour la config du bot");
+  createdChannel.send("Se salon a été créer pour config le bot")
   let createdChannel = await guild.channels.create("Delta-Log", {type: "text"})
   createdChannel.overwritePermissions([
     {
