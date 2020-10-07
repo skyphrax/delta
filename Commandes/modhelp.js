c10796c8573dd4d62f7d20066b449705e5c1e1f1
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-07 11:13:31 +0200

    Create modhelp.js

---
+++ Commandes/modhelp.js
@@ -0,0 +1,30 @@
+module.exports.run = (client,message, args) => {
+  let perm = message.member.hasPermission('MANAGE_MESSAGES')
+if(!perm) return message.channel.send("Tu n'as pas la permission de taper cette commande!")
+  const embed = new MessageEmbed()
+      .setColor("#dc143c") 
+      .setTitle("Liste des commandes de modérations")
+      .setURL("https://discord.gg/pJnP2pP")
+      .setDescription("Annonce à l'attention de tout le monde")
+      .setThumbnail(client.user.displayAvatarURL())
+      .setImage(client.user.displayAvatarURL())
+      .addField("Quel est ce bot?","Ce bot est le bot qui sera rendu public lors de la fin de son developpement.")
+      .addFields(
+      { name: 'Date du bot ', value: 'Le 2 Octobre 2020', inline: true},
+      { name: 'Fonctionnalité du Bot', value: 'Il sera polyvalent et pourra vous aidez dans plusieurs taches.Pour plus,contactez Kwartz Kreiz#0001', inline: true}
+
+      )
+
+
+.setTimestamp()
+.setFooter("Tout droit résérvé | DeltaBot ©️ 2020");
+
+message.channel.send(embed);
+
+
+
+    }
+
+  module.exports.help = {
+    name: 'modhelp',
+  };
