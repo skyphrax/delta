e52144476031e6a6b4a797715a15ebea56d24a98
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-07 11:14:10 +0200

    Update ban.js

--- Commandes/ban.js
+++ Commandes/ban.js
@@ -24,7 +24,8 @@ module.exports.run = async (client, message, args) => {
    . setTimestamp ( )
     .setFooter(`Tout droit résérvé | DeltaBot © ️ 2020` , client.user.displayAvatarURL)
 
-   message.channel.send(banEmbed)
+   const log_channel = client.channels.cache.find(`nom`, "delta-log")
+log_channel.send(banEmbed)
  
    bannedUser.ban({banReason})
 }
