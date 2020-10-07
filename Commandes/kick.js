07e9d0ab66c33a3be6f3a1ccefd0ddeee293df34
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-07 12:14:08 +0200

    Update kick.js

--- Commandes/kick.js
+++ Commandes/kick.js
@@ -4,8 +4,14 @@ const colours = require("../colours.json");
 module.exports.run = async (client, message, args) => {
  
    let kickedUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-   if(!kickedUser) {
-       return message.channel.send("**Not found.**")
+    if(!kickedUser) {
+       let helpembed = new Discord.MessageEmbed()
+   .setDescription("-Kick-")
+   .setColor(colours.red_light)
+   .addField("Mentionnez l'utilisateur", "```exemple: d!kick @steve ...```")
+   .addField("La raison", "```exemple:d!kick @... menace de ddos```")
+    .setFooter(`Tout droit résérvé | DeltaBot © ️ 2020` , client.user.displayAvatarURL)
+    message.channel.send(helpembed)
    }
    let kickReason = args.join(" ").slice(22);
    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
