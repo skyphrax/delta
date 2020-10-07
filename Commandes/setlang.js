9dd6159e42d53390943cd3ef8c2fd835168c2c28
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-07 16:48:49 +0200

    Create setlang.js

---
+++ Commandes/setlang.js
@@ -0,0 +1,42 @@
+const { languages } = require('../lang.json')
+const mongo = require('../mongo')
+const languageSchema = require('../language-schema.js')
+
+module.exports = {
+  commands: ['setlang', 'setlanguage'],
+  minArgs: 1,
+  maxArgs: 1,
+  expectedArgs: '<Language>',
+  permissions: 'ADMINISTRATOR'
+  callback: (message, arguments) => {
+    const { guild } = message
+    
+    const targetLanguage = arguments[0].toLowerCase()
+    if (!languages.includes(targetLanguage)){
+      message.reply('That language is not supported.')
+      return
+    }
+
+    await mongo().then(async (mongoose) => {
+      try {
+        await languageSchema.findOneAndUpdate({
+          _id: guild.id
+        }, {
+          _id: guild.id,
+          language: targetLanguage
+        }, {
+          upsert: true
+        })
+        
+        message.reply('Language set!').then(message) => {
+          const seconds = 3
+          message.delete({
+            timeout: 1000 * seconds
+          })
+        }
+      } finally {
+        mongoose.connection.close()
+      }
+    })
+  }
+}
