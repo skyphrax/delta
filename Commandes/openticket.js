bb25fc9f7b6945c562a8d304742c151cb46f86fc
skyphrax <72354810+skyphrax@users.noreply.github.com>
2020-10-08 12:04:58 +0200

    Update openticket.js

--- Commandes/openticket.js
+++ Commandes/openticket.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
 const {PREFIX} = require("../config.js");
 const colours = require("../colours.json")
+const db = require('quick.db')
 
 module.exports.run = async (client, message, args, member) => {
     message.delete()
     if(!message.member.hasPermission("ADMINISTRATOR")) return;
     let chd = db.get(`tickchannel_${member.guild.id}`);
+    
     let OpenTicket = new Discord.MessageEmbed()
     .setDescription('React `📥` for open a ticket')
       member.guild.channels.cache.get(chd).send(OpenTicket).then(msg => msg.react('📥'))
